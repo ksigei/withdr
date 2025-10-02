56fcf0462f97af743daa066d49a1d2bb8297e19e
ksigei <sigeikiprono4@gmail.com>
2025-10-02 13:49:37 +0300

    d

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-h=gxoq1tuebix+g$n1t$zz&+1a9e8khiicy8bp)))q-e&mj_&r'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["withdr-production.up.railway.app", "*"]
 
 
 # ---------------- INSTALLED APPS ----------------
