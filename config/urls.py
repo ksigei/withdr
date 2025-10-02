5d711a213fe2b8451e241c56d057a2cf275f9550
ksigei <sigeikiprono4@gmail.com>
2025-10-02 14:09:35 +0300

    Static fixces

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),   # login/signup/social login
+
+    # allauth routes (login, signup, logout, social login, etc.)
+    path("accounts/", include("allauth.urls")),
+
+    # your custom accounts app (if you add extra views later)
     path("", include(("accounts.urls", "accounts"), namespace="accounts")),
 ]
+
+# ✅ serve media in dev
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.BASE_DIR / "media")
